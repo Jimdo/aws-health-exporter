d789498c1336643735b175d0adb443168ec69d49
Simon Hartmann <simon.hartmann@jimdo.com>
2017-09-05 11:29:32 +0200

    Simplify test and make it more explicit

--- aws_health_exporter_test.go
+++ aws_health_exporter_test.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"log"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/health"
 	"github.com/aws/aws-sdk-go/service/health/healthiface"
-	"github.com/prometheus/client_golang/prometheus"
 	dto "github.com/prometheus/client_model/go"
 )
 
@@ -21,116 +21,59 @@ func (api *mockHealthAPI) DescribeEventsPages(in *health.DescribeEventsInput, fn
 	return nil
 }
 
-func TestCollect(t *testing.T) {
+func TestScrape(t *testing.T) {
 	var events = []*health.Event{
 		&health.Event{
 			AvailabilityZone:  aws.String(""),
 			EventTypeCategory: aws.String("issue"),
 			Region:            aws.String("eu-west-1"),
 			Service:           aws.String("EC2"),
+			StatusCode:        aws.String("open"),
+		},
+		&health.Event{
+			AvailabilityZone:  aws.String(""),
+			EventTypeCategory: aws.String("issue"),
+			Region:            aws.String("us-east-1"),
+			Service:           aws.String("EC2"),
+			StatusCode:        aws.String("open"),
+		},
+		&health.Event{
+			AvailabilityZone:  aws.String(""),
+			EventTypeCategory: aws.String("issue"),
+			Region:            aws.String("us-east-1"),
+			Service:           aws.String("LAMBDA"),
 			StatusCode:        aws.String("closed"),
 		},
 		&health.Event{
 			AvailabilityZone:  aws.String(""),
 			EventTypeCategory: aws.String("issue"),
-			Region:            aws.String("eu-west-1"),
-			Service:           aws.String("EC2"),
+			Region:            aws.String("us-east-1"),
+			Service:           aws.String("LAMBDA"),
 			StatusCode:        aws.String("closed"),
 		},
 	}
 	e := &exporter{
-		api: &mockHealthAPI{events: events},
-		filter: &health.EventFilter{
-			Regions: aws.StringSlice([]string{"us-west-1"}),
-		},
+		api:    &mockHealthAPI{events: events},
+		filter: &health.EventFilter{},
 	}
-	ch := make(chan prometheus.Metric)
 
-	go func() {
-		defer close(ch)
-		e.Collect(ch)
-	}()
+	e.scrape()
 
-	if expect, got := 2., readCounter((<-ch).(prometheus.Counter)); expect != got {
+	if expect, got := 2., readCounter("", "issue", "us-east-1", "LAMBDA", "closed"); expect != got {
 		t.Errorf("Counter 'closed' has wrong value. Expected: %v Got: %v", expect, got)
 	}
-}
-
-func TestFilter(t *testing.T) {
-	var events = []*health.Event{
-		&health.Event{
-			AvailabilityZone:  aws.String(""),
-			EventTypeCategory: aws.String("issue"),
-			Region:            aws.String("eu-west-1"),
-			Service:           aws.String("EC2"),
-			StatusCode:        aws.String("open"),
-		},
-		&health.Event{
-			AvailabilityZone:  aws.String(""),
-			EventTypeCategory: aws.String("issue"),
-			Region:            aws.String("us-west-1"),
-			Service:           aws.String("LAMBDA"),
-			StatusCode:        aws.String("open"),
-		},
-	}
-	e := &exporter{
-		api: &mockHealthAPI{events: events},
-		filter: &health.EventFilter{
-			Regions: aws.StringSlice([]string{"us-west-1"}),
-		},
-	}
-	ch := make(chan prometheus.Metric)
-
-	go func() {
-		defer close(ch)
-		e.Collect(ch)
-	}()
-
-	if expect, got := 1., readCounter((<-ch).(prometheus.Counter)); expect != got {
+	if expect, got := 1., readCounter("", "issue", "us-east-1", "EC2", "open"); expect != got {
 		t.Errorf("Counter 'open' has wrong value. Expected: %v Got: %v", expect, got)
 	}
 }
 
-func readCounter(m prometheus.Metric) float64 {
+func readCounter(availabilityZone, eventTypeCategory, region, service, statusCode string) float64 {
+	vec, ok := counters[statusCode]
+	if !ok {
+		log.Fatalf("no CounterVec for status code %v", statusCode)
+	}
+	m := vec.WithLabelValues(availabilityZone, eventTypeCategory, region, service)
 	pb := &dto.Metric{}
 	m.Write(pb)
 	return pb.GetCounter().GetValue()
-}
-
-var mockEvents = []*health.Event{
-	&health.Event{
-		AvailabilityZone:  aws.String(""),
-		EventTypeCategory: aws.String("issue"),
-		Region:            aws.String("eu-west-1"),
-		Service:           aws.String("EC2"),
-		StatusCode:        aws.String("closed"),
-	},
-	&health.Event{
-		AvailabilityZone:  aws.String(""),
-		EventTypeCategory: aws.String("issue"),
-		Region:            aws.String("us-east-1"),
-		Service:           aws.String("RDS"),
-		StatusCode:        aws.String("open"),
-	},
-	&health.Event{
-		AvailabilityZone:  aws.String(""),
-		EventTypeCategory: aws.String("issue"),
-		Region:            aws.String("us-east-1"),
-		Service:           aws.String("RDS"),
-		StatusCode:        aws.String("closed"),
-	},
-	&health.Event{
-		AvailabilityZone:  aws.String(""),
-		EventTypeCategory: aws.String("issue"),
-		Region:            aws.String("us-east-1"),
-		Service:           aws.String("RDS"),
-		StatusCode:        aws.String("closed"),
-	},
-	&health.Event{
-		AvailabilityZone:  aws.String(""),
-		EventTypeCategory: aws.String("issue"),
-		Region:            aws.String("us-west-1"),
-		Service:           aws.String("LAMBDA"),
-		StatusCode:        aws.String("upcoming"),
-	},
 }
