91b7e44c3233e02760ebbe633e6e8390d66b2d1e
javad-hajiani <javad.hajiani@deliveryhero.com>
2022-05-11 17:30:18 +0200

    Update aws-sdk-go to v1.44.11

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/Jimdo/aws-health-exporter
 go 1.17
 
 require (
-	github.com/aws/aws-sdk-go v1.10.37-0.20170831220942-44376742d0a0
+	github.com/aws/aws-sdk-go v1.44.11
 	github.com/prometheus/client_golang v0.9.0-pre1
 	github.com/prometheus/client_model v0.0.0-20170216185247-6f3806018612
 	github.com/prometheus/common v0.0.0-20170830190555-bc8b88226a12
@@ -18,7 +18,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-ini/ini v1.28.3-0.20170813052230-c787282c39ac // indirect
 	github.com/golang/protobuf v0.0.0-20170901042739-5afd06f9d81a // indirect
-	github.com/jmespath/go-jmespath v0.0.0-20160803190731-bd40a432e4c7 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/prometheus/procfs v0.0.0-20170703101242-e645f4e5aaa8 // indirect
 	github.com/smartystreets/goconvey v1.7.2 // indirect
--- vendor/github.com/aws/aws-sdk-go/service/health/doc.go
+++ vendor/github.com/aws/aws-sdk-go/service/health/doc.go
@@ -3,44 +3,48 @@
 // Package health provides the client and types for making API
 // requests to AWS Health APIs and Notifications.
 //
-// The AWS Health API provides programmatic access to the AWS Health information
-// that is presented in the AWS Personal Health Dashboard (https://phd.aws.amazon.com/phd/home#/).
-// You can get information about events that affect your AWS resources:
+// The Health API provides programmatic access to the Health information that
+// appears in the Personal Health Dashboard (https://phd.aws.amazon.com/phd/home#/).
+// You can use the API operations to get information about events that might
+// affect your Amazon Web Services services and resources.
 //
-//    * DescribeEvents: Summary information about events.
+//    * You must have a Business, Enterprise On-Ramp, or Enterprise Support
+//    plan from Amazon Web Services Support (http://aws.amazon.com/premiumsupport/)
+//    to use the Health API. If you call the Health API from an Amazon Web Services
+//    account that doesn't have a Business, Enterprise On-Ramp, or Enterprise
+//    Support plan, you receive a SubscriptionRequiredException error.
 //
-//    * DescribeEventDetails: Detailed information about one or more events.
+//    * You can use the Health endpoint health.us-east-1.amazonaws.com (HTTPS)
+//    to call the Health API operations. Health supports a multi-Region application
+//    architecture and has two regional endpoints in an active-passive configuration.
+//    You can use the high availability endpoint example to determine which
+//    Amazon Web Services Region is active, so that you can get the latest information
+//    from the API. For more information, see Accessing the Health API (https://docs.aws.amazon.com/health/latest/ug/health-api.html)
+//    in the Health User Guide.
 //
-//    * DescribeAffectedEntities: Information about AWS resources that are affected
-//    by one or more events.
+// For authentication of requests, Health uses the Signature Version 4 Signing
+// Process (https://docs.aws.amazon.com/general/latest/gr/signature-version-4.html).
 //
-// In addition, these operations provide information about event types and summary
-// counts of events or affected entities:
+// If your Amazon Web Services account is part of Organizations, you can use
+// the Health organizational view feature. This feature provides a centralized
+// view of Health events across all accounts in your organization. You can aggregate
+// Health events in real time to identify accounts in your organization that
+// are affected by an operational event or get notified of security vulnerabilities.
+// Use the organizational view API operations to enable this feature and return
+// event information. For more information, see Aggregating Health events (https://docs.aws.amazon.com/health/latest/ug/aggregate-events.html)
+// in the Health User Guide.
 //
-//    * DescribeEventTypes: Information about the kinds of events that AWS Health
-//    tracks.
+// When you use the Health API operations to return Health events, see the following
+// recommendations:
 //
-//    * DescribeEventAggregates: A count of the number of events that meet specified
-//    criteria.
+//    * Use the eventScopeCode (https://docs.aws.amazon.com/health/latest/APIReference/API_Event.html#AWSHealth-Type-Event-eventScopeCode)
+//    parameter to specify whether to return Health events that are public or
+//    account-specific.
 //
-//    * DescribeEntityAggregates: A count of the number of affected entities
-//    that meet specified criteria.
-//
-// The Health API requires a Business or Enterprise support plan from AWS Support
-// (http://aws.amazon.com/premiumsupport/). Calling the Health API from an account
-// that does not have a Business or Enterprise support plan causes a SubscriptionRequiredException.
-//
-// For authentication of requests, AWS Health uses the Signature Version 4 Signing
-// Process (http://docs.aws.amazon.com/general/latest/gr/signature-version-4.html).
-//
-// See the AWS Health User Guide (http://docs.aws.amazon.com/health/latest/ug/what-is-aws-health.html)
-// for information about how to use the API.
-//
-// Service Endpoint
-//
-// The HTTP endpoint for the AWS Health API is:
-//
-//    * https://health.us-east-1.amazonaws.com
+//    * Use pagination to view all events from the response. For example, if
+//    you call the DescribeEventsForOrganization operation to get all events
+//    in your organization, you might receive several page results. Specify
+//    the nextToken in the next request to return more results.
 //
 // See https://docs.aws.amazon.com/goto/WebAPI/health-2016-08-04 for more information on this service.
 //
@@ -49,7 +53,7 @@
 //
 // Using the Client
 //
-// To AWS Health APIs and Notifications with the SDK use the New function to create
+// To contact AWS Health APIs and Notifications with the SDK use the New function to create
 // a new service client. With that client you can make API requests to the service.
 // These clients are safe to use concurrently.
 //
